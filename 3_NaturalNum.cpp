dfaab4d117983aac973fdee4a45b1234a29e8813
Maksim <maks1102b@gmail.com>
2015-10-25 22:53:12 +0300

    Added prog

---
+++ 3_NaturalNum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <clocale>
+using std::cin;
+using std::cout;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int number1 = 0, number2 = 0, nc = 0, mc = 0, kc = 0, tc = 0, sum1 = 0, sum2 = 0;
+	cout << "¬ведите натуральное число: ";
+	cin >> number1;
+	nc = number2 = kc = number1;
+	while (nc > 0){
+		mc = nc % 10;
+		sum1 += mc;
+		nc = nc / 10;
+	}
+	while (sum1 != sum2){
+		sum2 = 0;
+		number2++;
+		kc = number2;
+		while (kc > 0){
+			tc = kc % 10;
+			sum2 += tc;
+			kc = kc / 10;
+		}
+	}
+	cout << number2 << "\n";
+	return 0;
+}
